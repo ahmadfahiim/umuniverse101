9404c832ac9be5941ea55dd8ea69a5de0f232596
Sishdoap <aimansha887@gmail.com>
2024-12-31 16:00:37 +0800

    fixed image
    fixed image view for the welcome page

--- app/src/main/java/com/example/firebase/MainActivityJava.java
+++ app/src/main/java/com/example/firebase/MainActivityJava.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +22,12 @@ public class MainActivityJava extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.welcome_page); // Ensure the layout file is named correctly
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
 // User model class
