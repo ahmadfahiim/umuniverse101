6c6ec7b5dd302794a3e6b73a5f598a4c709bd10e
ahmadfahiim <fahiimkamal123@gmail.com>
2025-01-01 16:29:55 +0800

    testing github

---
+++ app/src/main/java/com/example/firebase/EventPage.java
@@ -0,0 +1,14 @@
+package com.example.firebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_page);
+    }
+}
--- app/src/main/java/com/example/firebase/HomePage.java
+++ app/src/main/java/com/example/firebase/HomePage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -30,7 +31,7 @@ public class HomePage extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         logoutBtn = findViewById(R.id.logout);
         user = auth.getCurrentUser();
-        textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.user_email);
 
         if (user == null) {
             Intent intent = new Intent(getApplicationContext(), Login.class);
@@ -45,12 +46,21 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
                 finish();
             }
         });
 
+        ImageView eventsButton = findViewById(R.id.events_button);
+        eventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EventPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 
