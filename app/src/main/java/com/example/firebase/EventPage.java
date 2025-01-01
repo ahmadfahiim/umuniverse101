8a132bf99c8a751492a9af8bcba4b58c29013168
Sishdoap <aimansha887@gmail.com>
2025-01-01 17:59:07 +0800

    fixed the event page navigation

--- app/src/main/java/com/example/firebase/EventPage.java
+++ app/src/main/java/com/example/firebase/EventPage.java
@@ -1,8 +1,11 @@
 package com.example.firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EventPage extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class EventPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_page);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationBar);
+        bottomNavigationView.setSelectedItemId(R.id.events);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                Intent homeIntent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(homeIntent);
+            } else if (itemId == R.id.events) {
+                return true;
+            } else if (itemId == R.id.bookings) {
+                // To do
+                return true;
+            } else if (itemId == R.id.profile) {
+                Intent profileIntent = new Intent(getApplicationContext(), ProfilePage.class);
+                startActivity(profileIntent);
+                return true;
+            }
+            return false;
+
+        });
     }
 }
